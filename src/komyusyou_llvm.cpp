4a5a97f7cef5dde0a99bcb3347fd51bf128d2eb6
kokkiemouse <kokkiemouse@fascode.net>
2021-10-08 17:36:18 +0900

    noproblem

--- src/komyusyou_llvm.cpp
+++ src/komyusyou_llvm.cpp
@@ -3,9 +3,15 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/LLVMContext.h>
 #include <llvm/IR/Module.h>
+static llvm::LLVMContext TheContext;
+static llvm::IRBuilder<> Builder(TheContext);
+static std::unique_ptr<llvm::Module> TheModule;
 int main(){
-    char* data=(char*)calloc(32767,sizeof(char));
-    char* ptr=data;
-    free(data);
-    return 0;
+    TheModule = llvm::make_unique<llvm::Module>("top", TheContext);
+    llvm::Function* mainFunc = llvm::Function::Create(
+        llvm::FunctionType::get(llvm::Type::getInt32Ty(TheContext), false),
+        llvm::Function::ExternalLinkage, "main", TheModule.get());
+    Builder.SetInsertPoint(llvm::BasicBlock::Create(TheContext, "", mainFunc));
+    Builder.CreateRet(Builder.getInt32(42));
+    TheModule->dump();   
 }
